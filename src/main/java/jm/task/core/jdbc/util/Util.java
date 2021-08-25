e3dc02a8ccab0581b2bca9718f3cdee503463645
Konstantin Narishkin <narishun@gmail.com>
2021-08-25 12:32:43 +0300

    Ready

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,18 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+
+    private static final String URL = "jdbc:mysql://localhost:3306/mysqltest?useSSL=false";
+    private static final String USER = "root";
+    private static final String PASS = "root";
+
+    public static Connection getMySQLConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(URL, USER, PASS);
+    }
 }
