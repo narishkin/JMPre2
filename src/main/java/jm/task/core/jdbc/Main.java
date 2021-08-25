1bdc7b0bd01e9fc86a45fc1251b433d676e95568
Konstantin Narishkin <narishun@gmail.com>
2021-08-25 14:22:41 +0300

    Ready

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -7,18 +7,19 @@ public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
         UserServiceImpl userServiceImpl = new UserServiceImpl();
+        userServiceImpl.createUsersTable();
+
         User user1 = new User("Mark", "Benz", (byte)29);
         User user2 = new User("Peter", "Kotov", (byte)23);
         User user3 = new User("Christopher", "Dudin", (byte)67);
         User user4 = new User("Ivan", "Draga", (byte)50);
 
-        userServiceImpl.createUsersTable();
         userServiceImpl.saveUser(user1.getName(),user1.getLastName(),user1.getAge());
         userServiceImpl.saveUser(user2.getName(),user2.getLastName(),user2.getAge());
         userServiceImpl.saveUser(user3.getName(),user3.getLastName(),user3.getAge());
         userServiceImpl.saveUser(user4.getName(),user4.getLastName(),user4.getAge());
+
         System.out.println(userServiceImpl.getAllUsers().toString());
-        userServiceImpl.removeUserById(2);
         userServiceImpl.cleanUsersTable();
         userServiceImpl.dropUsersTable();
     }
